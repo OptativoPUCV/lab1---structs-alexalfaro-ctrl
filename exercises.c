fcafb0ba8dbd14cb4d219a958ba20e4575f2e2b1
Replit user <>
2023-08-17 21:05:25 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -141,6 +141,6 @@ Nodo *crearListaEnlazada(int arr[], int size) {
   for (int i=0 ;i<size;i++){
     Nodo*hola=(Nodo*)malloc(sizeof(Nodo));
     hola->numero=arr[i];
-    hola->nodo->siguiente=NULL;
+    hola->siguiente=NULL;
   }
 }
